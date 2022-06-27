f8aa97a7e1875b5cf5c15c9630388402cd6e997b
Vamsi <vamsisunkaranam@gmail.com>
2022-06-27 11:16:37 +0530

    extracted get_available_options logic into a method

--- py stuff/21.py
+++ py stuff/21.py
@@ -1,21 +1,22 @@
-
 import random
 
-total = 0
-while True:
+def get_avaiable_option(total):
     available_options = []
     current_option = 1
     while (total + current_option) <= 21 and current_option <= 3:
         available_options.append(current_option)
         current_option += 1
+    return available_options
 
+total = 0
+while True:
     invalid_input = False
     try:
         user_int = int(input("Your Number? "))
     except Exception:
         invalid_input = True
 
-    if user_int not in available_options or invalid_input:
+    if user_int not in get_avaiable_option(total) or invalid_input:
         print('Enter Valid Input.')
         continue
     total = total + user_int
@@ -23,11 +24,7 @@ while True:
         print("YOU LOOSE!!!")
         exit()
 
-    available_options = []
-    current_option = 1
-    while (total + current_option) <= 21 and current_option <= 3:
-        available_options.append(current_option)
-        current_option += 1
+    available_options = get_avaiable_option(total)
 
     comp_int = random.randint(min(available_options),max(available_options))
     print('Computer Played:', comp_int)
