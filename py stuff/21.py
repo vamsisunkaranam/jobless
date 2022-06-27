11e28cd11455387a4c35c7690cc5716e90259121
Vamsi <vamsisunkaranam@gmail.com>
2022-06-27 11:01:38 +0530

    upataded 21: computer smrter, can evaluate available options

--- py stuff/21.py
+++ py stuff/21.py
@@ -3,23 +3,36 @@ import random
 
 total = 0
 while True:
+    available_options = []
+    current_option = 1
+    while (total + current_option) <= 21 and current_option <= 3:
+        available_options.append(current_option)
+        current_option += 1
+
     invalid_input = False
     try:
-        user_int = int(input("\nYour Number?\n"))
+        user_int = int(input("Your Number? "))
     except Exception:
         invalid_input = True
 
-    if user_int not in [1, 2, 3] or invalid_input:
+    if user_int not in available_options or invalid_input:
         print('Enter Valid Input.')
         continue
     total = total + user_int
-    if total>=21:
+    if total >= 21:
         print("YOU LOOSE!!!")
         exit()
 
-    comp_int = random.randint(1,3)
-    print('\nComputer Played.\n ', comp_int)
+    available_options = []
+    current_option = 1
+    while (total + current_option) <= 21 and current_option <= 3:
+        available_options.append(current_option)
+        current_option += 1
+
+    comp_int = random.randint(min(available_options),max(available_options))
+    print('Computer Played:', comp_int)
     total = total + comp_int
     if total>=21:
         print("YOU WIN!!!")
         exit()
+    print("Total:", total, "\n")
